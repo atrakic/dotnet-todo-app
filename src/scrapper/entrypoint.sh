d6bf2b7ad650c29b2b20939583f14a8fe47f0058
Admir Trakic <atrakic@users.noreply.github.com>
2023-11-04 02:27:13 +0100

    Add scrapper

---
+++ src/scrapper/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+  
+API_PORT=${API_PORT:-80}
+HOST=${HOST:-api}
+SLEEP=5
+
+while true; do
+
+  title=$(echo $(for((i=1;i<=13;i++)); do printf '%s' "${RANDOM:0:1}"; done) | tr '[0-9]' '[a-z]')
+  feed=$(curl -s -X POST https://lipsum.com/feed/json -d "amount=1" -d "what=lines" -H 'accept: text/plain' | jq -r '.feed.lipsum')
+  scraped_json=$(cat <<-END
+  {
+  "title": "$title",
+  "description": "$feed",
+  "isDone": false
+  }
+END
+)
+
+  id=$(curl -s -X 'POST' \
+    -H 'accept: */*' \
+    -H 'Content-Type: application/json' \
+    -d "$scraped_json" \
+    http://"${HOST}":"${API_PORT}"/todos/v1 |jq -r '.id')
+
+  curl -s -X 'GET' \
+    -H 'accept: application/json' \
+    http://"${HOST}":"${API_PORT}"/todos/v1/"$id" | jq -r
+
+  sleep "$SLEEP"
+
+done
--- tests/e2e.sh
+++ tests/e2e.sh
@@ -2,27 +2,11 @@
 set -e
 set -o pipefail
 
-scraped_json=$(cat <<-END
-    {
-    "title": "$(echo $RANDOM)",
-    "description": "$(curl -s -X POST https://lipsum.com/feed/json -d "amount=1" -d "what=lines" -H 'accept: text/plain' | jq -r '.feed.lipsum')",
-    "isDone": false
-    }
-END
-)
-
-
+HOST=localhost
 API_PORT=5000
-
-curl -fik -X 'POST' \
-  -H 'accept: */*' \
-  -H 'Content-Type: application/json' \
-  -d "$scraped_json" \
-  http://localhost:${API_PORT}/todos/v1
 
 curl -s -X 'GET' \
   -H 'accept: application/json' \
-  http://localhost:${API_PORT}/todos/v1 | jq -r
-
+  http://"${HOST}":"${API_PORT}"/todos/v1 | jq -r
 
 docker exec -it db psql -h localhost -U postgres -d lipsum-api -c 'select count(*) from "Todos";'
