267a91a40e0ab5a1cf5d8ed11f64f9c6e44fc937
atrakic <atrakic@users.noreply.github.com>
2024-07-15 11:05:03 +0200

    chore(web): paginated results

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -1,43 +1,65 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-    const [data, setData]  = useState('');
+    const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(5);
+    
     const getAllData = () => {
         axios.get(process.env.REACT_APP_API_URL)
-          .then((response) => {
-            console.log(response.data);
-            setData(response.data);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-    }
+            .then((response) => {
+                console.log(response.data);
+                setData(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
     useEffect(() => {
-      getAllData();
-    },[]);
-  
+        getAllData();
+    }, []);
+
+    const lastItemIndex = currentPage * itemsPerPage;
+    const firstItemIndex = lastItemIndex - itemsPerPage;
+    const currentItems = data.slice(firstItemIndex, lastItemIndex);
+
     const displayData = () => {
-      return data ? (
-        data.map((data) => {
-          return (
-            <div className="data" key={data.id}>
-              <h3>{data.title}</h3>
-              <p>{data.description}</p>
-              <hr />
-            </div>
-          );
-        })
-      ) : (
-        <h3>No API data from: {process.env.REACT_APP_API_URL}</h3>
-      );
-    }
+        return currentItems.length ? (
+            currentItems.map((item) => (
+                <div className="data" key={item.id}>
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                    <hr />
+                </div>
+            ))
+        ) : (
+            <h3>No API data from: {process.env.REACT_APP_API_URL}</h3>
+        );
+    };
+
+    const totalPages = Math.ceil(data.length / itemsPerPage);
+
+    const goToNextPage = () => {
+        setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+    };
+
+    const goToPreviousPage = () => {
+        setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+    };
+
     return (
-      <>
-        {displayData()}
-      </>
+        <>
+            {displayData()}
+            <div className="pagination">
+                <button onClick={goToPreviousPage} disabled={currentPage === 1}>Prev</button>
+                <span>Page {currentPage} of {totalPages}</span>
+                <button onClick={goToNextPage} disabled={currentPage === totalPages}>Next</button>
+            </div>
+        </>
     );
-  }
+}
 
 export default App;
